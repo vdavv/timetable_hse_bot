675e94cd5fcd461b9a74f12047c657f7d8bc2d14
vdavv <vdavvv@ya.ru>
2022-11-22 12:31:15 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import telebot
+import config
+import time
+import datetime
+
+bot = telebot.TeleBot(config.BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(commands=['timer'])
+def timer(message):
+    print(message.chat.id)
+    print(message)
+    n = 5
+    time.sleep(n)
+    bot.reply_to(message, f'Прошло {n} секунд{message.id}')
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    print(message.chat.id)
+    # print(message)
+    print(message.text + ' - ' + message.from_user.username + " - " + datetime.datetime.now().strftime("%H:%M:%S"))
+    if message.text[0] == '/':
+        bot.reply_to(message, 'command unknown')
+
+    else:
+        bot.reply_to(message, 'howdy?')
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
---
+++ parser.py
@@ -0,0 +1,2 @@
+from bs4 import BeautifulSoup
+# timetable sheet: https://docs.google.com/spreadsheets/d/1-DzUiDC11i98lK6i1v870pKLCRdCYodcHtF_f0h1FQY/edit#gid=247034206
